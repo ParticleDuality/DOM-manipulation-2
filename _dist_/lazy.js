4a47bd41e22e355a099737e3b6938c35ede0e3e4
ParticleDuality <ParticleDuality@users.noreply.github.com>
2022-05-20 03:46:12 +0000

    deploy: bfbe6d988044deb29bf45cd5dccbfb152ce0de95

---
+++ _dist_/lazy.js
@@ -0,0 +1,16 @@
+const observer = new IntersectionObserver((entries) => {
+  entries
+    .filter((entry) => entry.isIntersecting)
+    .forEach((entry) => {
+      console.log(entry)
+      let container = entry.target
+      let image = container.firstChild
+      
+      image.src = image.dataset.src
+      observer.unobserve(container)
+    })
+})
+
+export const registerImage = (img) => {
+  observer.observe(img)
+}
